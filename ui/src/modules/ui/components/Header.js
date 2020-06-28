0d350c77ee927f4dbcd78c8ea9c2c9fe9744aa39
Nuttavut Thongjor <n.thongjor@gmail.com>
2020-06-28 22:37:44 +0700

    Add UI

---
+++ ui/src/modules/ui/components/Header.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import {
+  AppBar,
+  Toolbar,
+  Link,
+  FormControlLabel,
+  Switch,
+  Badge,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { ShoppingCart } from '@material-ui/icons'
+
+import logo from 'assets/images/logo.png'
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    zIndex: theme.zIndex.drawer,
+  },
+  logoLink: {
+    marginRight: theme.spacing(2),
+  },
+  logoImage: {
+    widht: '30px',
+    height: '30px',
+  },
+  spacer: {
+    flexGrow: 1,
+  },
+}))
+
+export default function Header() {
+  const classes = useStyles()
+
+  return (
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar>
+        <Link
+          to="/"
+          color="inherit"
+          underline="none"
+          className={classes.logoLink}
+        >
+          <img src={logo} alt="Babel Shopping" className={classes.logoImage} />
+        </Link>
+        <Link to="/products" color="inherit" underline="none">
+          Products
+        </Link>
+        <div className={classes.spacer}></div>
+        <FormControlLabel
+          control={<Switch color="secondary" />}
+          label="Dark"
+          labelPlacement="end"
+        ></FormControlLabel>
+        <Badge badgeContent={5} color="secondary">
+          <ShoppingCart></ShoppingCart>
+        </Badge>
+      </Toolbar>
+    </AppBar>
+  )
+}
---
+++ ui/src/modules/ui/components/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { CssBaseline } from '@material-ui/core'
+
+import Header from './Header'
+import Content from './Content'
+
+export default function Layout() {
+  return (
+    <>
+      <CssBaseline></CssBaseline>
+      <Header></Header>
+      <Content></Content>
+    </>
+  )
+}
