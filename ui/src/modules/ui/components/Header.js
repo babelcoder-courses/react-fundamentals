0e46e6b6b4b2cbed4f79b9a839e5880b8b210d3c
Nuttavut Thongjor <n.thongjor@gmail.com>
2020-06-30 01:29:09 +0700

    Use router link

--- ui/src/modules/ui/components/Header.js
+++ ui/src/modules/ui/components/Header.js
@@ -6,7 +6,9 @@ import {
   FormControlLabel,
   Switch,
   Badge,
+  IconButton,
 } from '@material-ui/core'
+import { Link as RouterLink } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import { ShoppingCart } from '@material-ui/icons'
 
@@ -35,6 +37,7 @@ export default function Header() {
     <AppBar position="fixed" className={classes.appBar}>
       <Toolbar>
         <Link
+          component={RouterLink}
           to="/"
           color="inherit"
           underline="none"
@@ -42,7 +45,12 @@ export default function Header() {
         >
           <img src={logo} alt="Babel Shopping" className={classes.logoImage} />
         </Link>
-        <Link to="/products" color="inherit" underline="none">
+        <Link
+          component={RouterLink}
+          to="/products"
+          color="inherit"
+          underline="none"
+        >
           Products
         </Link>
         <div className={classes.spacer}></div>
@@ -51,9 +59,11 @@ export default function Header() {
           label="Dark"
           labelPlacement="end"
         ></FormControlLabel>
-        <Badge badgeContent={5} color="secondary">
-          <ShoppingCart></ShoppingCart>
-        </Badge>
+        <IconButton color="inherit">
+          <Badge badgeContent={5} color="secondary">
+            <ShoppingCart></ShoppingCart>
+          </Badge>
+        </IconButton>
       </Toolbar>
     </AppBar>
   )
