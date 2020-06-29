8fa8cec4e9e725906e435f329e4e61f3eedfc6c8
Nuttavut Thongjor <n.thongjor@gmail.com>
2020-06-29 22:08:16 +0700

    Use useRouteMatch instead of adding path manually

--- ui/src/modules/cart/components/Routes.js
+++ ui/src/modules/cart/components/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, useRouteMatch } from 'react-router-dom'
 
 import Cart from './Cart'
 
 export default function Routes() {
+  const { path } = useRouteMatch()
+
   return (
     <Switch>
-      <Route path="/cart">
+      <Route path={path}>
         <Cart></Cart>
       </Route>
     </Switch>
