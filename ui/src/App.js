a4c9d7a2bed55dc9707fce1c29ed5713dd07494b
Nuttavut Thongjor <n.thongjor@gmail.com>
2020-06-30 04:02:11 +0700

    Add ability to validate form with yup

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
 
 export default function App() {
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, errors } = useForm({
+    mode: 'onBlur',
     defaultValues: {
       gender: 'Male',
     },
+    validationSchema: yup.object().shape({
+      email: yup.string().required(),
+      password: yup.string().min(8).required(),
+      gender: yup.mixed().oneOf(['Male', 'Female']),
+    }),
   })
 
   const submit = (value) => {
@@ -15,8 +22,11 @@ export default function App() {
   return (
     <form onSubmit={handleSubmit(submit)}>
       <input type="text" name="email" ref={register} />
+      {errors.email && <div>{errors.email.message}</div>}
       <input type="password" name="password" ref={register} />
+      {errors.password && <div>{errors.password.message}</div>}
       <input type="text" name="gender" ref={register} />
+      {errors.gender && <div>{errors.gender.message}</div>}
       <button type="submit">Submit</button>
     </form>
   )
