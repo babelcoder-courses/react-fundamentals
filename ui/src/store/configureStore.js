346b99c71c32ba7d6f2401b8e35a5fa58f08a263
Nuttavut Thongjor <n.thongjor@gmail.com>
2020-07-02 00:49:34 +0700

    Configure production build

--- ui/src/store/configureStore.js
+++ ui/src/store/configureStore.js
@@ -1,23 +1,7 @@
-import { createStore, applyMiddleware } from 'redux'
-import { createBrowserHistory } from 'history'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { routerMiddleware } from 'connected-react-router'
-import reduxThunk from 'redux-thunk'
-
-import createRootReducer from 'modules/reducers'
-
-export const history = createBrowserHistory()
-
-function configureStore(initialState) {
-  const middleware = [reduxThunk, routerMiddleware(history)]
-
-  const store = createStore(
-    createRootReducer(history),
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
-  )
-
-  return store
+switch (process.env.NODE_ENV) {
+  case 'production':
+    module.exports = require('./configureStore.prod')
+    break
+  default:
+    module.exports = require('./configureStore.dev')
 }
-
-export default configureStore
